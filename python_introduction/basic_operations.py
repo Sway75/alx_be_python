3a0c1d829479b146adf35209357bbdcf0a6a04a4
Sway <godswaydk75@gmail.com>
2025-11-07 10:58:35 +0000

    git commit -m "basic_operations.py"

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -2,7 +2,7 @@ number1 = 10
 number2 = 5
 sum_result = number1 + number2
 difference_result = number1 - number2
-product_result = number1 * number
+product_result = number1 * number2
 print(sum_result)
 print(difference_result)
 print(product_result)
