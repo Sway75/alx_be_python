f82e6b358604384c2d01520e15a4b74caf3df04c
Sway <godswaydk75@gmail.com>
2026-01-20 18:54:12 +0000

    multiplication_table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,13 @@
+# user = int(input("Enter the number: "))
+
+# for x in range(1, 11, 1):
+    
+#     multi_user = x * user
+#     print(f"{x} * {user} = {multi_user}")
+
+number = int(input("Enter a number to see its multiplication table: "))
+
+for i in range(1, 11):
+    product = number * i
+    print(f"{number} * {i} = {product}")
+
