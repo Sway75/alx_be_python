1b2e8b7dfa7dc331b3dcc150d782133d2e26c644
Sway <godswaydk75@gmail.com>
2026-03-11 22:32:34 +0000

    operations_function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1:float, num2:float, operation:str):
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return "Invalid operation"
